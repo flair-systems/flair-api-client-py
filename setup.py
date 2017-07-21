57138ec35b62d9dbc3c56706a11d40618a4a8b4c
Edward Paget <edward@cassetteta.pe>
2017-07-21 13:40:44 -0500

    Bump verison for PyPI release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='flair-client',
-      version='0.0.3',
+      version='0.1.0',
       description='Flair API Client',
       author='Edward Paget',
       author_email='ed@flair.co',
