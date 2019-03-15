66ab4f3725be0a53e3d05f9769e22d9f93b72817
Edward Paget <ed.paget@gmail.com>
2019-03-15 15:43:34 -0400

    Release version 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='flair-client',
-      version='0.2.0',
+      version='1.0.0',
       description='Flair API Client',
       author='Edward Paget',
       author_email='ed@flair.co',
